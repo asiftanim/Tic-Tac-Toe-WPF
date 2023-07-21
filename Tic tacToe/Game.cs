234751ad70e674eb9b1a1bc6f555891a02192111
Asif Tanim <tanimasif23@gmail.com>
2023-07-21 07:33:32 -0400

    Initial commit

---
+++ Tic tacToe/Game.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_tacToe
+{
+    internal class Game
+    {
+        private const string X = "X";
+        private const string O = "O";
+        private string CurrentPlayer { get; set; } = String.Empty;
+        private string[,] GameBoard = new string[3, 3];
+        private int TurnCount = 1;
+
+        public Game() 
+        {
+            CurrentPlayer = X;
+        }
+
+        public void UpdateGameBoard(string buttonPosition)
+        {
+            var position = buttonPosition.Split(',');
+            int xVal = int.Parse(position[0]);
+            int yVal = int.Parse(position[1]);
+            GameBoard[xVal, yVal] = CurrentPlayer;
+            TurnCount++;
+        }
+
+        public int getTurnCount()
+        {
+            return TurnCount;
+        }
+
+        public void setNextPlayer()
+        {
+            CurrentPlayer = CurrentPlayer == X ? O : X;
+        }
+
+        public string getCurrentPlayer()
+        {
+            return CurrentPlayer;
+        }
+
+        public bool CheckWinCondition()
+        {
+            //Vertical
+            for(int i = 0; i < 3; i++) 
+            {
+                if (!String.IsNullOrWhiteSpace(GameBoard[i, 0]))
+                {
+                    if (GameBoard[i, 0] == GameBoard[i, 1] && GameBoard[i, 0] == GameBoard[i, 2])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            //Horizontal
+            for (int i = 0; i < 3; i++)
+            {
+                if (!String.IsNullOrWhiteSpace(GameBoard[0, i]))
+                {
+                    if (GameBoard[0, i] == GameBoard[1, i] && GameBoard[0, i] == GameBoard[2, i])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            
+            if (!String.IsNullOrWhiteSpace(GameBoard[1, 1]))
+            {
+                //Diagonal
+                if (GameBoard[0, 0] == GameBoard[1, 1] && GameBoard[1, 1] == GameBoard[2, 2])
+                {
+                    return true;
+                }
+
+                //Anti Diagonal
+                if (GameBoard[0, 2] == GameBoard[1, 1] && GameBoard[1, 1] == GameBoard[2, 0])
+                {
+                    return true;
+                }
+
+            }
+
+
+
+            return false;
+        }
+
+        public void resetGame()
+        {
+            CurrentPlayer = X;
+            GameBoard = new string[3, 3];
+            TurnCount = 1;
+        }
+    }
+}
---
+++ Tic tacToe/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tic_tacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Game game;
+        public MainWindow()
+        {
+            InitializeComponent();
+            game = new Game();
+        }
+
+        private void Board_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            
+            if(button.Name != "NewGame")
+            {
+                button.Content = game.getCurrentPlayer();
+
+                if (game.getTurnCount() == 9)
+                {
+                    Notification.Content = "Match Draw!!!";
+                    DisableAllBoardButtons();
+                }
+                else
+                {
+                    game.UpdateGameBoard(button.Tag.ToString());
+                    if (game.CheckWinCondition())
+                    {
+                        Notification.Content = game.getCurrentPlayer() + " Won !!!";
+                        DisableAllBoardButtons();
+                    }
+                    game.setNextPlayer();
+                }
+            }
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            game.resetGame();
+            ResetButton();
+        }
+
+        private void ResetButton()
+        {
+            foreach (Button button in buttonGrid.Children)
+            {
+                button.Content = String.Empty;
+                button.IsEnabled = true;
+            }
+            Notification.Content = "";
+        }
+
+        private void DisableAllBoardButtons()
+        {
+            foreach (Button button in buttonGrid.Children)
+            {
+                button.IsEnabled = false;
+            }
+        }
+    }
+}
