0d9247ec6e6125527cfca8ae225d8d057b4d1471
Asif Tanim <tanimasif23@gmail.com>
2024-10-15 19:02:46 -0400

    Update Game.cs

--- Tic tacToe/Game.cs
+++ Tic tacToe/Game.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace Tic_tacToe
 {
@@ -10,13 +11,20 @@ namespace Tic_tacToe
     {
         private const string X = "X";
         private const string O = "O";
+
+        private const int MatrixLength = 3;
+
+        private readonly int[] NumberRange;
+
+
         private string CurrentPlayer { get; set; } = String.Empty;
-        private string[,] GameBoard = new string[3, 3];
+        private string[,] GameBoard = new string[MatrixLength, MatrixLength];
         private int TurnCount = 1;
 
         public Game() 
         {
             CurrentPlayer = X;
+            NumberRange = Enumerable.Range(0, MatrixLength).ToArray();
         }
 
         public void UpdateGameBoard(string buttonPosition)
@@ -46,7 +54,10 @@ namespace Tic_tacToe
         public bool CheckWinCondition()
         {
             //Vertical
-            for(int i = 0; i < 3; i++) 
+            //|
+            //|
+            //|
+            for(int i = 0; i < MatrixLength; i++) 
             {
                 if (!String.IsNullOrWhiteSpace(GameBoard[i, 0]))
                 {
@@ -58,6 +69,7 @@ namespace Tic_tacToe
             }
 
             //Horizontal
+            //-------------
             for (int i = 0; i < 3; i++)
             {
                 if (!String.IsNullOrWhiteSpace(GameBoard[0, i]))
@@ -73,12 +85,18 @@ namespace Tic_tacToe
             if (!String.IsNullOrWhiteSpace(GameBoard[1, 1]))
             {
                 //Diagonal
+                //\
+                // \
+                //  \
                 if (GameBoard[0, 0] == GameBoard[1, 1] && GameBoard[1, 1] == GameBoard[2, 2])
                 {
                     return true;
                 }
 
                 //Anti Diagonal
+                //   /
+                //  /
+                // /
                 if (GameBoard[0, 2] == GameBoard[1, 1] && GameBoard[1, 1] == GameBoard[2, 0])
                 {
                     return true;
